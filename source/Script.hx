6656645f2de4db371b93fa43910f6b9242a1f029
Fearester <111678905+Fearester@users.noreply.github.com>
2022-10-01 23:46:49 +0300

    Update Script.hx

--- source/Script.hx
+++ source/Script.hx
@@ -1,96 +1,68 @@
-package song;
+package;
 
-#if android
-import android.Hardware;
-import android.Permissions;
-import android.os.Build;
-import android.os.Environment;
-#end
-import flixel.FlxBasic;
 import hscript.Interp;
-import openfl.Lib;
-#if sys
+
+import PlayState;  
+import Discord;
+import Character;
+import Boyfriend;
+import Song;
+import GameOverSubstate;
+import HealthIcon;
 import sys.FileSystem;
 import sys.io.File;
-#end
+import haxe.Json;
+import Section;
+import StrumNote;
+import ClientPrefs;
+import Conductor;
+import Note; 
+import MainMenuState; 
+import NoteSplash; 
 
-class Script extends FlxBasic
-{
-	public var hscript:Interp;
+import flixel.FlxBasic;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxGame;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.FlxSubState;
+import flixel.ui.FlxBar;
+import flixel.util.FlxCollision;
+import flixel.util.FlxColor;
+import flixel.util.FlxSort;
+import flixel.util.FlxStringUtil;
+import flixel.util.FlxTimer;
+import flixel.addons.display.FlxBackdrop;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 
-	public override function new()
-	{
-		super();
-		hscript = new Interp();
-	}
-
-	public function runScript(script:String)
-	{
-		var parser = new hscript.Parser();
-
-		try
-		{
-			var ast = parser.parseString(script);
-
-			hscript.execute(ast);
-		}
-		catch (e)
-		{
-			Lib.application.window.alert(e.message, "HSCRIPT ERROR!1111");
-		}
-	}
-
-	public function setVariable(name:String, val:Dynamic)
-	{
-		hscript.variables.set(name, val);
-	}
-
-	public function getVariable(name:String):Dynamic
-	{
-		return hscript.variables.get(name);
-	}
-
-	public function executeFunc(funcName:String, ?args:Array<Any>):Dynamic
-	{
-		if (hscript == null)
-			return null;
-
-		if (hscript.variables.exists(funcName))
-		{
-			var func = hscript.variables.get(funcName);
-			if (args == null)
-			{
-				var result = null;
-				try
-				{
-					result = func();
-				}
-				catch (e)
-				{
-					trace('$e');
-				}
-				return result;
-			}
-			else
-			{
-				var result = null;
-				try
-				{
-					result = Reflect.callMethod(null, func, args);
-				}
-				catch (e)
-				{
-					trace('$e');
-				}
-				return result;
-			}
-		}
-		return null;
-	}
-
-	public override function destroy()
-	{
-		super.destroy();
-		hscript = null;
-	}
+class HscriptHandler {
+    public static function setVars(interp:Interp) {
+        interp.variables.set('PlayState', PlayState);
+        interp.variables.set('Character', Character);
+        interp.variables.set('Paths', Paths);
+        interp.variables.set('Boyfriend', Boyfriend);
+        interp.variables.set('HealthIcon', HealthIcon);
+        interp.variables.set('StrumNote', StrumNote);
+        interp.variables.set('Conductor', Conductor);
+        interp.variables.set('ClientPrefs', ClientPrefs);
+        interp.variables.set('GameOverSubstate', GameOverSubstate);
+        interp.variables.set('Note', Note);
+        interp.variables.set('FlxG', FlxG);
+        interp.variables.set('MainMenuState', MainMenuState);
+        interp.variables.set('Song', Song);
+        interp.variables.set('FlxGame', FlxGame);
+        interp.variables.set('FlxBackdrop', FlxBackdrop);
+        interp.variables.set('FlxBar', FlxBar);
+        interp.variables.set('Section', Section);
+        interp.variables.set('FlxState', FlxState);
+        interp.variables.set('FlxEase', FlxEase);
+        interp.variables.set('FlxTween', FlxTween);
+        interp.variables.set('NoteSplash', NoteSplash);
+        interp.variables.set('FlxSprite', FlxSprite);
+        interp.variables.set('FlxBasic', FlxBasic);
+        return interp;
+    }
 }
