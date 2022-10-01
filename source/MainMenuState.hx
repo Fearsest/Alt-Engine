324b901ae0167579a5bd8f2c5eb8f927505fa6ad
Fearester <111678905+Fearester@users.noreply.github.com>
2022-10-01 13:14:51 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,6 +26,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.6.2'; //This is also used for Discord RPC
+        public static var altEngineVersion:String = '1.5.0';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
@@ -130,15 +131,17 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Alt Engine Version: " + psychEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Alt Engine Version: " + altEngineVersion, 12);
 		versionShit.scrollFactor.set();
+
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' Version: " + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
-
+                var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine Version: " + altEngineVersion, 12);
+		versionShit.scrollFactor.set();
 		// NG.core.calls.event.logEvent('swag').send();
 
 		changeItem();
