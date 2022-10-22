28c07e163c50fb387e4ed0807d4458447abb11aa
Fearester <111678905+Fearester@users.noreply.github.com>
2022-10-22 21:59:56 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -112,7 +112,7 @@ class MainMenuState extends MusicBeatState
 		add(camFollow);
 		add(camFollowPos);
 
-		magenta = new FlxSprite(-80).loadGraphic(Paths.image(MainJSON.BackGround));
+		magenta = new FlxSprite(-80).loadGraphic(Paths.image(MainJSON.menuBG));
 		magenta.scrollFactor.set(0, yScroll);
 		magenta.setGraphicSize(Std.int(magenta.width * 1.175));
 		magenta.updateHitbox();
