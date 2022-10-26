563cc024f40104eaee48a7c734a518a80a0d90de
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2022-10-26 08:48:14 +0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -166,7 +166,7 @@ class FreeplayState extends MusicBeatState
         scoreText = new FlxText(FreeplayJSON.ScoreTextP[0],FreeplayJSON.ScoreTextP[1], 0,FreeplayJSON.FreeplayScoreText, FreeplayJSON.FreeplayScoreTextSize);
 		scoreText.setFormat(Paths.font("vcr.ttf"), FreeplayJSON.FreeplayScoreTextSize, FlxColor.WHITE, RIGHT);
 
-		scoreBG = new FlxSprite(FreeplayJSON.FreeplayScoreBGPos[0], FreeplayJSON.FreeplayScoreBGPos[1]).makeGraphic(1, 1, FreeplayJSON.ScoreBGColor);
+		scoreBG = new FlxSprite(FreeplayJSON.FreeplayScoreBGPos[0], FreeplayJSON.FreeplayScoreBGPos[1]).makeGraphic(1, 1,"0x" + FreeplayJSON.ScoreBGColor);
 		scoreBG.alpha = FreeplayJSON.ScoreBGA;
 		scoreBG.scale.x = FreeplayJSON.FreeplayScoreBGScale[0];
 		scoreBG.scale.y = FreeplayJSON.FreeplayScoreBGScale[1];
