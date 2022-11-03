db675fcdd98f59a12e545190de9c1049ac19fff5
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2022-11-03 20:55:47 +0300

    Update ScriptState.hx

--- source/ScriptState.hx
+++ source/ScriptState.hx
@@ -32,12 +32,6 @@ class ScriptState extends MusicBeatState
 		daScript.executeFunc('new', args);
 	}
 
-	public override function onCursorEvent(code:String, target:IFlxUIWidget)
-	{
-		daScript.executeFunc("onCursorEvent", [code, target]);
-		super.onCursorEvent(code, target);
-	}
-
 	#if !mobile
 	public override function onDropFile(path:String)
 	{
@@ -62,12 +56,6 @@ class ScriptState extends MusicBeatState
 	{
 		daScript.executeFunc("onResize", [width, height]);
 		super.onResize(width, height);
-	}
-
-	public override function onShowTooltip(t:FlxUITooltip)
-	{
-		daScript.executeFunc("onShowTooltip", [t]);
-		super.onShowTooltip(t);
 	}
 
 	public override function draw()
