38c9a3273c7f4b2309650240a5a6dfb38a967191
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2022-11-03 22:24:25 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -137,7 +137,7 @@ class PlayState extends MusicBeatState
 	public static var storyWeek:Int = 0;
 	public static var storyPlaylist:Array<String> = [];
 	public static var storyDifficulty:Int = 1;
-
+    public static var scriptArray:Array<ScriptCore> = [];
 	public var spawnTime:Float = 2000;
 
 	public var vocals:FlxSound;
