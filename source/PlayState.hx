0c388d676a771f1bfc61b2c7414cc61cc77b033f
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2022-11-04 22:03:11 +0300

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4907,14 +4907,16 @@ class PlayState extends MusicBeatState
 		}
 		luaArray = [];
 
+        #if hscript
+		if(FunkinLua.hscript != null) FunkinLua.hscript = null;
+		#end
+		
 		if(!ClientPrefs.controllerMode)
 		{
 			FlxG.stage.removeEventListener(KeyboardEvent.KEY_DOWN, onKeyPress);
 			FlxG.stage.removeEventListener(KeyboardEvent.KEY_UP, onKeyRelease);
 		}
-		#if hscript
-		FunkinLua.haxeInterp = null;
-		#end
+		
 		super.destroy();
 	}
 
